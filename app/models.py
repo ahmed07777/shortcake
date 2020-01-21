67ef09f9d885b04272912b87d50b8b6d5e315731
David Udelson <dru5@cornell.edu>
2020-01-21 16:59:56 -0500

    Debug database creation and add flask shell context

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 
 class ShortURL(db.Model):
+    __tablename__ = 'shortURL'
     key = db.Column(db.String(10), primary_key=True)
     url = db.Column(db.String(1000))
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import os
 from app import create_app
+from app.db import db
+from app.models import ShortURL
 from config import config
 
 app = create_app(os.getenv('FLASK_CONFIG') or config['default'])
+with app.app_context():
+    db.create_all()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'ShortURL': ShortURL
+    }
